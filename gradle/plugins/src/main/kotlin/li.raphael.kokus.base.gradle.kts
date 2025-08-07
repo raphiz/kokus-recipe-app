cdda43232523d1d3913e8a0fd86edff0a9790846
Raphael Zimmermann <mail@raphael.li>
2025-08-07 06:50:22 +0200

    chore: Update to gradle 9.0.0
    Gradle produces reproducible archives by default now

--- gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
+++ gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
@@ -1,12 +1,3 @@
-
-// Produce reproducible archives, see https://github.com/raphiz/buildGradleApplication?tab=readme-ov-file#rule-6-tell-gradle-to-be-more-reproducible
-tasks.withType<AbstractArchiveTask>().configureEach {
-    isPreserveFileTimestamps = false
-    isReproducibleFileOrder = true
-    dirPermissions { unix("755") }
-    filePermissions { unix("644") }
-}
-
 // Fail build when dynamic dependency versions or Changing versions (SNAPSHOTS, etc) are used
 configurations.configureEach {
     resolutionStrategy {
