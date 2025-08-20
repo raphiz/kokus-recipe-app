c79b99189ba860cf7da413fb0000d2cc800971a9
Raphael Zimmermann <mail@raphael.li>
2025-08-20 07:11:55 +0200

    build: Exclude known "troublemakers" from intellij

--- gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
+++ gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
@@ -1,6 +1,26 @@
+plugins {
+    idea
+}
+
 // Fail build when dynamic dependency versions or Changing versions (SNAPSHOTS, etc) are used
 configurations.configureEach {
     resolutionStrategy {
         failOnNonReproducibleResolution()
+    }
+}
+
+idea {
+    module {
+        excludeDirs.addAll(
+            setOf(
+                ".gradle",
+                ".pre-commit-config.yaml",
+                "result",
+                ".idea",
+                ".kotlin",
+                "build",
+                "out",
+            ).map { file(it) },
+        )
     }
 }
