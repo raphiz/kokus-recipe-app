350133e942fd26287d1825d542405b878b785c2b
Raphael Zimmermann <mail@raphael.li>
2025-08-24 16:21:12 +0200

    build: Don't use typesafe-conventions as it breaks the update-verification-metadata task

--- gradle/plugins/settings.gradle.kts
+++ gradle/plugins/settings.gradle.kts
@@ -4,16 +4,16 @@ pluginManagement {
     }
 }
 
-plugins {
-    // Sadly, it's not posisble to use a plugin declared in a version catalog in a settings file
-    id("dev.panuszewski.typesafe-conventions") version "0.7.4"
-}
-
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    versionCatalogs {
+        create("libs") {
+            from(files("../libs.versions.toml"))
+        }
+    }
 }
 
 rootProject.name = "kokus-plugins"
--- gradle/plugins/src/main/kotlin/li.raphael.kokus.kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/li.raphael.kokus.kotlin.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    kotlin("jvm")
 }
 
 kotlin {
@@ -9,11 +9,15 @@ kotlin {
     }
 }
 
+val Project.libs get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+operator fun VersionCatalog.get(name: String) = findLibrary(name).get()
+
 dependencies {
-    testImplementation(platform(libs.junit.bom))
-    testImplementation(libs.junit.jupiter.api)
-    testRuntimeOnly(libs.junit.platform.launcher)
-    testRuntimeOnly(libs.junit.jupiter.engine)
+    testImplementation(platform(libs["junit-bom"]))
+    testImplementation(libs["junit-jupiter-api"])
+    testRuntimeOnly(libs["junit-platform-launcher"])
+    testRuntimeOnly(libs["junit-jupiter-engine"])
 }
 
 tasks.test {
