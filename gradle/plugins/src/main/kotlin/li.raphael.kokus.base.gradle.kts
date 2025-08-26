730bfbcff1c2b30736c312d032a77cde42d68ca3
Raphael Zimmermann <mail@raphael.li>
2025-08-26 08:06:54 +0200

    build: Add eager canary task

--- gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
+++ gradle/plugins/src/main/kotlin/li.raphael.kokus.base.gradle.kts
@@ -9,6 +9,25 @@ configurations.configureEach {
     }
 }
 
+tasks.register("eagerCanary") {
+    group = "verification"
+    description =
+        """
+        if this configuration is executed, an eager API is used
+        see https://docs.gradle.org/current/userguide/task_configuration_avoidance.html#eager_apis_to_avoid        
+        """.trimIndent()
+
+    if (gradle.startParameter.taskNames.none { it == "tasks" || it.endsWith(":tasks") }) {
+        throw IllegalStateException(
+            """
+            Eager task configuration detected: All tasks were realized during configuration phase.
+            To avoid this, use lazy configuration APIs.
+            See https://docs.gradle.org/current/userguide/task_configuration_avoidance.html#eager_apis_to_avoid for details.
+            """.trimIndent(),
+        )
+    }
+}
+
 idea {
     module {
         excludeDirs.addAll(
