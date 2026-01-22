ed693ee096e2ce2eb06032d07338a10fc97bfb8e
Raphael Zimmermann <mail@raphael.li>
2026-01-22 05:37:14 +0100

    refactor: Remove reflection from IdFactory

--- app/fundamentals/domain/src/main/kotlin/li/raphael/kokus/fundamentals/Id.kt
+++ app/fundamentals/domain/src/main/kotlin/li/raphael/kokus/fundamentals/Id.kt
@@ -1,31 +1,29 @@
 package li.raphael.kokus.fundamentals
 
-import java.lang.reflect.Constructor
 import java.util.UUID
-import kotlin.reflect.KClass
 
 data class RecipeId(
     override val value: UUID,
 ) : Id {
-    companion object : IdFactory<RecipeId>(RecipeId::class)
+    companion object : IdFactory<RecipeId>(::RecipeId)
 }
 
 data class MealId(
     override val value: UUID,
 ) : Id {
-    companion object : IdFactory<MealId>(MealId::class)
+    companion object : IdFactory<MealId>(::MealId)
 }
 
 interface Id {
     val value: UUID
 }
 
-abstract class IdFactory<ID : Id>(
-    private val idConstructor: Constructor<ID>,
+abstract class IdFactory<T : Id>(
+    private val factory: (UUID) -> T,
 ) {
-    constructor(idKlass: KClass<ID>) : this(idKlass.java.getConstructor(UUID::class.java))
+    fun random(): T = factory(UUID.randomUUID())
 
-    private fun createId(value: UUID): ID = idConstructor.newInstance(value)
+    fun from(uuid: UUID): T = factory(uuid)
 
-    fun random(): ID = createId(UUID.randomUUID())
+    fun from(string: String): T = factory(UUID.fromString(string))
 }
